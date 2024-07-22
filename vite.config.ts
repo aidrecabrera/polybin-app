47c7e1250c13422dd6bc5f1d9bafcb8c24e45c44
Svene <hello.svene@gmail.com>
2024-07-22 09:26:37 +0800

    chore: Update service worker registration and manifest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     VitePWA({
       strategies: 'injectManifest',
       srcDir: 'src',
-      filename: 'sw.js',
+      filename: '/public/sw.js',
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       manifest: {
